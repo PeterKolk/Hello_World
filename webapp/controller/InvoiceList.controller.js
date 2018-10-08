255cb8df70611468eecb1da0158bf006fce2a790
Kolk P. <peter.kolk@nn-group.com>
2018-10-08 12:49:34 +0000

    Step 23 ->  Custom formatters -> formats the status text to be readable

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"HelloWorld/HelloWorld/model/formatter"
+], function (Controller, JSONModel, formatter) {
 	"use strict";
 
 	return Controller.extend("HelloWorld.HelloWorld.controller.InvoiceList", {
-
+		formatter: formatter,
 		onInit : function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
