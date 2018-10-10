74c7e061a1c651ee0f61fd4fdd33ed2dd69c4183
Kolk P. <peter.kolk@nn-group.com>
2018-10-10 15:03:14 +0000

    navigation added to app

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -28,6 +28,13 @@ sap.ui.define([
 			oBinding.filter(aFilter);	
 			
 			
+		},
+		onPress: function (oEvent) {
+			var oItem = oEvent.getSource();
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("detail", {
+				invoicePath: oItem.getBindingContext("invoice").getPath().substr(1)
+			});
 		}
 
 	});
